69d7e3c95e35d6e9696dc56295baa46403e5d9ef
Manuel Šarić <manujelko@gmail.com>
2022-11-18 17:26:02 +0100

    Add and register template.

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 MONTH_NOT_SUPPORTED = HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
@@ -36,7 +37,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
+        response_data = render_to_string("challenges/challenge.html")
     except KeyError:
         return MONTH_NOT_SUPPORTED
 
