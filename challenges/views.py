94895f02a20f8a88a3dc3bc8e706d9d500b4d80a
Manuel Šarić <manujelko@gmail.com>
2022-11-18 16:30:56 +0100

    Use reverse.

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -22,11 +23,12 @@ monthly_challenges = {
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     try:
-        forward_month = months[month - 1]
+        redirect_month = months[month - 1]
     except IndexError:
         return HttpResponseNotFound("This month is not supported!")
 
-    return HttpResponseRedirect(f"/challenges/{forward_month}")
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
